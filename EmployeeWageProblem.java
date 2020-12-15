50d92bfc6df84e9e748abcac4f1df46d26c1a9d5
uthsa <uthsavi29@gmail.com>
2020-12-15 09:51:26 +0530

    Modified:To Calculate Daily Wage Of Employee

--- EmployeeWageProblem.java
+++ EmployeeWageProblem.java
@@ -1,14 +1,20 @@
 public class EmployeeWageProblem {
 	public static void main(String[] args) {
 		System.out.println("Welcome To Employee Wage Computation");
-				// Constants
+			// Constants
 		int IS_FULL_TIME = 1;
-					// Computation
+		int EMP_RATE_PER_HOUR = 20;
+			//Variables
+		int empHrs = 0;
+		int empWage = 0;
+			// Computation
 		double empCheck = Math.floor(Math.random() * 10) % 2;
 		if (empCheck == IS_FULL_TIME)
-			System.out.println("Employee Is Present");
+			empHrs = 8;
 		else
-			System.out.println("Employee Is Absent");
+			empHrs = 0;
+		empWage = empHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Emp Wage:" + empWage);
 				
 	}
 }

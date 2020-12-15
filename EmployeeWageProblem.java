af337ef08a016d6e1e970e7ad8049d0e666a851a
uthsa <uthsavi29@gmail.com>
2020-12-15 09:41:15 +0530

    Added:To Check Employee Attendance

--- EmployeeWageProblem.java
+++ EmployeeWageProblem.java
@@ -1,5 +1,14 @@
 public class EmployeeWageProblem {
 	public static void main(String[] args) {
 		System.out.println("Welcome To Employee Wage Computation");
+				// Constants
+		int IS_FULL_TIME = 1;
+					// Computation
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if (empCheck == IS_FULL_TIME)
+			System.out.println("Employee Is Present");
+		else
+			System.out.println("Employee Is Absent");
+				
 	}
 }

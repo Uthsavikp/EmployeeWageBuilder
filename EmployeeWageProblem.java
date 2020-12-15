4ac4ec64ff472685e0c36c8bc9b3ea0c63814d05
uthsa <uthsavi29@gmail.com>
2020-12-15 10:00:25 +0530

    Refactor:To Add Part Time Employee And Wage

--- EmployeeWageProblem.java
+++ EmployeeWageProblem.java
@@ -1,15 +1,18 @@
 public class EmployeeWageProblem {
 	public static void main(String[] args) {
 		System.out.println("Welcome To Employee Wage Computation");
-			// Constants
-		int IS_FULL_TIME = 1;
+		// Constants
+		int IS_PART_TIME = 1;
+		int IS_FULL_TIME = 2;
 		int EMP_RATE_PER_HOUR = 20;
-			//Variables
+		// Variables
 		int empHrs = 0;
 		int empWage = 0;
-			// Computation
-		double empCheck = Math.floor(Math.random() * 10) % 2;
-		if (empCheck == IS_FULL_TIME)
+		// Computation
+		double empCheck = Math.floor(Math.random() * 10) % 3;
+		if (empCheck == IS_PART_TIME)
+			empHrs = 4;
+		else if (empCheck == IS_FULL_TIME)
 			empHrs = 8;
 		else
 			empHrs = 0;
@@ -18,3 +21,4 @@ public class EmployeeWageProblem {
 				
 	}
 }
+			

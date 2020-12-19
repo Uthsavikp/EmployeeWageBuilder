64ac9e219713b8f34151e5ed4749ee3313e7322c
uthsa <uthsavi29@gmail.com>
2020-12-19 12:14:14 +0530

    Refactor:To Write Class Method To Compute Employee Wage

--- EmployeeWageProblem.java
+++ EmployeeWageProblem.java
@@ -1,15 +1,14 @@
-public class EmployeeWageProblem {
-
+public class EmployeeWageProblem { 
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
 	public static final int EMP_RATE_PER_HOUR = 20;
 	public static final int NUM_OF_WORKING_DAYS = 2;
 	public static final int MAX_HRS_IN_MONTH = 10;
 	
-	public static void main(String[] args) {
+	public static int computeEmpWage() {
 		System.out.println("Welcome To Employee Wage Computation");
 		// Variables
-		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays =0;
+		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
 		// Computation
 		while (totalEmpHrs <= MAX_HRS_IN_MONTH &&
 				totalWorkingDays < NUM_OF_WORKING_DAYS){
@@ -30,8 +29,10 @@ public class EmployeeWageProblem {
 		}
 		int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
 		System.out.println("Total Emp Wage: " + totalEmpWage);
+		return totalEmpWage;
+	}
+	public static void main(String[] args) {
+		computeEmpWage();
 	}
 	
-	
 }
- 
